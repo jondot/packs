bebe536a885f77a7ca8e7a27403aa2642b3a4210
Zach Bagnall <zach@metabroadcast.com>
2014-06-27 15:47:37 +0100

    Add perspective transform option.
    Uses imagemagick distort. Outputs a PNG (regardless of source image type).

--- packimg/packimg.rb
+++ packimg/packimg.rb
@@ -12,13 +12,20 @@ class PackImg < Sinatra::Base
     
     return "" unless params[:source]
   
-    etag Digest::MD5.hexdigest("#{params[:source]}:#{params[:resize]}:#{params[:flip]}:#{params[:rotate]}:#{params[:format]}:#{params[:quality]}:#{params[:version]}")
+    etag Digest::MD5.hexdigest("#{params[:source]}:#{params[:resize]}:#{params[:flip]}:#{params[:perspective]}:#{params[:rotate]}:#{params[:format]}:#{params[:quality]}:#{params[:version]}")
     img = Image.read(params[:source]).first
     
     ops = []
     ops << ->(image) { image }
     ops << ->(image) { image.resize_to_fit(*params[:resize].split('x'))} if params[:resize]
     ops << ->(image) { params[:flip] == 'vertical' ? image.flip : image.flop } if params[:flip]
+
+    if params[:perspective]
+      img.format = "png"
+      img.virtual_pixel_method = Magick::TransparentVirtualPixelMethod
+      ops << ->(image) { image.distort(Magick::PerspectiveDistortion, params[:perspective].split(',').collect{|x| x.to_f}) }
+    end
+
     ops << ->(image) { image.rotate(params[:rotate].to_i) } if params[:rotate]
 
     res = ops.inject(img){|o,proc| proc.call(o)}
